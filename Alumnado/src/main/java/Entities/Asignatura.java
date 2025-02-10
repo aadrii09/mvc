69e93e0ac954ae4b84e61cd6f5584a04ea0a2e51
Adrián <145448579+aadrii09@users.noreply.github.com>
2025-02-10 09:28:39 +0100

    delete y buscar por id

--- Alumnado/src/main/java/Entities/Asignatura.java
+++ Alumnado/src/main/java/Entities/Asignatura.java
@@ -3,6 +3,8 @@ package Entities;
 
 import java.util.List;
 
+import org.hibernate.annotations.ManyToAny;
+
 import Entities.ENUMS.Tipo;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -13,6 +15,8 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
@@ -32,7 +36,9 @@ public class Asignatura {
     @Column(nullable = false, columnDefinition = "ENUM('OBLIGATORIA','OPCIONAL')")
     private Tipo tipo;
     
-    
+    // @ManyToMany(fetch = FetchType.EAGER, cascade=CascadeType.ALL)
+    // @JoinColumn(name = "alumnoasignatura", joinColumn=@JoinColumn(name="alumno_id"), inverseJoinColum=@JoinColumn(name="asignatura"))
+
     @OneToMany(mappedBy = "asignatura", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<AlumnoAsignatura> listaAlumnoasignaturas;
 
